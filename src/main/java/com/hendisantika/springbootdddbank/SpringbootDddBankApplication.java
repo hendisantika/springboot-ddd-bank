91605883d9d4692948be47045618890c397db36d
Hendi Santika <hendisantika@gmail.com>
2020-02-27 06:49:22 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdddbank/SpringbootDddBankApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdddbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDddBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDddBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootdddbank/SpringbootDddBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdddbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDddBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
