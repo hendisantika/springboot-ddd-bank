55c7c95daf2795895964391960b20dd6aa4f0440
Hendi Santika <hendisantika@gmail.com>
2020-02-27 06:55:54 +0700

    #9 - Add EntityBase Domain

---
+++ src/main/java/com/hendisantika/springbootdddbank/domain/Account.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdddbank.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/02/20
+ * Time: 06.54
+ */
+public class Account {
+}
