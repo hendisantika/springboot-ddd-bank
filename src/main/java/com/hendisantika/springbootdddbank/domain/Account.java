a59035d371d0db2aa191ca65002ccb26055f78fb
Hendi Santika <hendisantika@gmail.com>
2020-02-27 06:57:21 +0700

    #11 - Add Account Domain

--- src/main/java/com/hendisantika/springbootdddbank/domain/Account.java
+++ src/main/java/com/hendisantika/springbootdddbank/domain/Account.java
@@ -1,5 +1,9 @@
 package com.hendisantika.springbootdddbank.domain;
 
+import com.hendisantika.springbootdddbank.domain.base.EntityBase;
+
+import javax.persistence.Entity;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-ddd-bank
@@ -9,5 +13,51 @@ package com.hendisantika.springbootdddbank.domain;
  * Date: 27/02/20
  * Time: 06.54
  */
-public class Account {
+@Entity
+public class Account extends EntityBase<Account> {
+
+    private String name;
+    private Amount balance = new Amount(0);
+
+    /**
+     * Necessary for JPA entities internally.
+     */
+    @SuppressWarnings("unused")
+    private Account() {
+    }
+
+    public Account(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the minimum balance for the application.
+     *
+     * @return The minimum balance, which must stay on each account.
+     */
+    public static final Amount getMinimumBalance() {
+        return new Amount(-1000, 0);
+    }
+
+    public AccountNo accountNo() {
+        return new AccountNo(getId());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Account{accountNo=%d, name='%s', balance='%s'}", accountNo(), name, balance);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Amount getBalance() {
+        return balance;
+    }
+
+    void setBalance(final Amount amount) {
+        this.balance = amount;
+    }
+
 }
