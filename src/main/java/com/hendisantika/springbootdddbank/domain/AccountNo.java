e074a1b07d2bd9c03105a865656465cd11a0bdb2
Hendi Santika <hendisantika@gmail.com>
2020-03-01 06:54:23 +0700

    #25 - BUg Fix AccountNo Domain

--- src/main/java/com/hendisantika/springbootdddbank/domain/AccountNo.java
+++ src/main/java/com/hendisantika/springbootdddbank/domain/AccountNo.java
@@ -2,6 +2,8 @@ package com.hendisantika.springbootdddbank.domain;
 
 import multex.Exc;
 
+import static multex.MultexUtil.create;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-ddd-bank
