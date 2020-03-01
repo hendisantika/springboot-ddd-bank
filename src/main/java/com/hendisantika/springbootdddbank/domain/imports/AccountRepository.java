252babf9ec3bfdaa3ae0d470e3044d86d0e13c6d
Hendi Santika <hendisantika@gmail.com>
2020-03-02 06:54:16 +0700

    #43 - Add find in AccountRepository

--- src/main/java/com/hendisantika/springbootdddbank/domain/imports/AccountRepository.java
+++ src/main/java/com/hendisantika/springbootdddbank/domain/imports/AccountRepository.java
@@ -1,5 +1,10 @@
 package com.hendisantika.springbootdddbank.domain.imports;
 
+import com.hendisantika.springbootdddbank.domain.Account;
+import com.hendisantika.springbootdddbank.domain.AccountNo;
+
+import java.util.Optional;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-ddd-bank
@@ -10,4 +15,12 @@ package com.hendisantika.springbootdddbank.domain.imports;
  * Time: 06.53
  */
 public interface AccountRepository {
+    /**
+     * Searches the {@link Account} object with the given account number.
+     *
+     * @param acccountNo unique account number of the searched account
+     * @return the {@link Account} object with the given account number, if existing.
+     * @throws IllegalArgumentException acccountNo is null or empty
+     */
+    Optional<Account> find(AccountNo acccountNo);
 }
