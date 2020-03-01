d45fac49cf04b659690f1dca9b21e35c76b07f0a
Hendi Santika <hendisantika@gmail.com>
2020-03-02 06:55:17 +0700

    #46 - Add ClientRepository

---
+++ src/main/java/com/hendisantika/springbootdddbank/domain/imports/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdddbank.domain.imports;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/03/20
+ * Time: 06.55
+ */
+public interface ClientRepository {
+}
