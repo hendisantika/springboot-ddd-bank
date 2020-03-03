793099c198ea92d13c9788d8241573d05de945e9
Hendi Santika <hendisantika@gmail.com>
2020-03-04 06:45:06 +0700

    #58 - Add ClientJpaRepository

---
+++ src/main/java/com/hendisantika/springbootdddbank/infrastructure/ClientJpaRepository.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootdddbank.infrastructure;
+
+import com.hendisantika.springbootdddbank.domain.imports.ClientRepository;
+import com.hendisantika.springbootdddbank.infrastructure.imports.ImportedClientJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/03/20
+ * Time: 06.44
+ */
+@Service
+public class ClientJpaRepository implements ClientRepository {
+
+    private final ImportedClientJpaRepository impl;
+
+    @Autowired
+    public ClientJpaRepository(final ImportedClientJpaRepository impl) {
+        this.impl = impl;
+    }
+
+}
