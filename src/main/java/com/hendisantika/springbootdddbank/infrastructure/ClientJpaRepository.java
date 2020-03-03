8e09eb213ba5cc3d50b502c7786139b1d3a50e85
Hendi Santika <hendisantika@gmail.com>
2020-03-04 06:46:35 +0700

    #64 - Add findAll in ClientJpaRepository

--- src/main/java/com/hendisantika/springbootdddbank/infrastructure/ClientJpaRepository.java
+++ src/main/java/com/hendisantika/springbootdddbank/infrastructure/ClientJpaRepository.java
@@ -6,6 +6,7 @@ import com.hendisantika.springbootdddbank.infrastructure.imports.ImportedClientJ
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -47,6 +48,10 @@ public class ClientJpaRepository implements ClientRepository {
     @Override
     public Optional<Client> find(String username) {
         return impl.findOneByUsername(username);
+    }
+
+    public List<Client> findAll() {
+        return impl.findAllByOrderByIdDesc();
     }
 
 }
