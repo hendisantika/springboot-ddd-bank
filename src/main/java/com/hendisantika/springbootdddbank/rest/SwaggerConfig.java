f7b923727c3f5f38a292dfc7f3b9d0592e983e8c
Hendi Santika <hendisantika@gmail.com>
2020-03-05 06:48:11 +0700

    #82 - Add apiInfo in SwaggerConfig

--- src/main/java/com/hendisantika/springbootdddbank/rest/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootdddbank/rest/SwaggerConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by IntelliJ IDEA.
@@ -43,5 +46,27 @@ public class SwaggerConfig {
                 .build()
                 .apiInfo(apiInfo())
                 .securitySchemes(Arrays.asList(new BasicAuth("basicAuth")));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring DDD Bank REST Webservice",
+                "<p>A sample project following Domain Driven Design with Spring Data JPA</p>" +
+                        "<p>Usernames are predefined. The banker's username is <b>bank</b></p>" +
+                        "<p>Client usernames are <b>" + _clientUsernames() + "</b></p>" +
+                        "<p>Each user's password is equal to the username. </p>" +
+                        "<p>URIs under <b>/bank/</b> are for bankers, under <b>/client/</b> are for clients.</p>" +
+                        "<p>Keep in mind to login via the <b>Authorize</b> button before calling API methods.</p>" +
+                        "<p>Click on <b>application-controller</b> in order to see the API methods.</p>" +
+                        "",
+                null,
+                null,
+                new Contact(
+                        "Christoph Knabe", "http://public.beuth-hochschule.de/~knabe/", null
+                ),
+                null,
+                null,
+                Collections.emptyList()
+        );
     }
 }
