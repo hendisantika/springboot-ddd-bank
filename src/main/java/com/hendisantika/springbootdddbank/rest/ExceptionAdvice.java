bb113b688cc683cf6142886b30b3b7a99704ba3c
Hendi Santika <hendisantika@gmail.com>
2020-03-05 06:49:01 +0700

    #84 - Add ExceptionAdvice

---
+++ src/main/java/com/hendisantika/springbootdddbank/rest/ExceptionAdvice.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootdddbank.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/03/20
+ * Time: 06.48
+ */
+@ControllerAdvice
+public class ExceptionAdvice {
+}
