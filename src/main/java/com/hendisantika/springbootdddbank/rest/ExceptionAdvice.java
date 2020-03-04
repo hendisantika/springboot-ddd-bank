f37f14b0aa0ce7d8c46eb2b1800d679532d31e29
Hendi Santika <hendisantika@gmail.com>
2020-03-05 06:49:53 +0700

    #86 - Add reportException in ExceptionAdvice

--- src/main/java/com/hendisantika/springbootdddbank/rest/ExceptionAdvice.java
+++ src/main/java/com/hendisantika/springbootdddbank/rest/ExceptionAdvice.java
@@ -1,8 +1,16 @@
 package com.hendisantika.springbootdddbank.rest;
 
+import multex.Msg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.mediatype.vnderrors.VndErrors;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,5 +41,43 @@ public class ExceptionAdvice {
     public static final String BASE_NAME = "MessageText";
 
     private static final Logger log = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ResponseBody
+    @ExceptionHandler({Exception.class})
+    /**
+     * Reports the given Exception with messages in three ways:
+     * <ol>
+     * <li>with messages in default language and with stack trace to the error
+     * log</li>
+     * <li>with localized messages according to the given Locale of the web request
+     * to the REST client</li>
+     * <li>as HTTP status code to the REST client</li>
+     * </ol>
+     */
+    VndErrors reportException(final Exception ex, final Locale requestLocale, final HttpServletResponse response) {
+        // prepare messages for REST client with the Locale of the request:
+        /** Message texts for exceptions. */
+        final ResourceBundle requestResourceBundle = ResourceBundle.getBundle(BASE_NAME, requestLocale);
+        final StringBuffer clientMessages = new StringBuffer();
+        multex.Msg.printMessages(clientMessages, ex, requestResourceBundle);
+        final String clientMesagesString = clientMessages.toString();
+
+        // prepare log report with messages and stack trace:
+        final StringBuffer serverMessages = new StringBuffer();
+        serverMessages.append("Processing REST request threw exception:\n");
+        final Locale defaultLocale = Locale.getDefault();
+        final ResourceBundle defaultResourceBundle = ResourceBundle.getBundle(BASE_NAME, defaultLocale);
+        if (!defaultResourceBundle.equals(requestResourceBundle)) {
+            serverMessages.append(clientMesagesString);
+            serverMessages.append("\n-----\n");
+        }
+        Msg.printReport(serverMessages, ex, defaultResourceBundle);
+
+        // log the report on the server:
+        log.error(serverMessages.toString());
+        // respond with localized messages to the client:
+        response.setStatus(exceptionToStatus(ex).value());
+        return new VndErrors("error", clientMesagesString);
+    }
 
 }
